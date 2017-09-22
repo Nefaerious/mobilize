80c76fb48fcd12cb04a4687daec6ae286b456a4f
nefaerious <samshin102@gmail.com>
2017-09-22 13:57:18 -0500

    In working state

---
+++ extension/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".content").click(function(){
+        $(".calendar").fadeToggle("3000");
+    });
+});
+
+$(document).ready(function(){
+  $("#time-header").fadeIn("slow");
+});
