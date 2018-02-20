34bc85c827a4b1470dfc6d0867b14c445a3f3243
nefaerious <samshin102@gmail.com>
2018-02-20 13:28:12 -0600

    added xhr requesting for images

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,20 +1,22 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-};
 
-function getRandomImage()){
-  xhttp.open("GET", "unsplash.com/photos/random", true);
-  xhttp.send();
+function getRandomImage(){
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = requestStateChangeHandler;
+  xhr.open("GET", "https://api.unsplash.com/photos/random/", true);
+  xhr.setRequestHeader("Authorization", "Client-ID 5db66184bd1158b9377247c8fec383d3d7b187e39343a5a1584eaced7d4323c8");
+  xhr.setRequestHeader("w", "1920");
+  xhr.setRequestHeader("h", "1080");
+  xhr.setRequestHeader("orientation", "landscape");
+  xhr.send();
 }
 
-function setCollection(){
-
-}
-
-function getUser(){
-
+function requestStateChangeHandler(){
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.response);
+    var obj = JSON.parse(JSON.stringify(this.response));
+    var imageURL = obj.urls.full;
+    console.log(imageURL);
+    document.getElementById("image").style.backgroundImage = "url('"+imageURL+"')";
+  }
 }
